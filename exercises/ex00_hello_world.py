b99b3b8bb7a94449d62130883ac3ddecde50da0f
Yazan Alhassan <yazan@live.unc.edu>
2023-01-19 01:39:47 -0500

    Update ex00_hello_world.py
    Completed Hello World

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,4 +1,4 @@
+"""My first program for Comp100, UNC."""
+__author__ = "730326641"
 print("Hello, world.")
 print("Hello, world. I am Yazan.")
-docstring= "My first program for Comp100, UNC."
-__author_v="730326641"
